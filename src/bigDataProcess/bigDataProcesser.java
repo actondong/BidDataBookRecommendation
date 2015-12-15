344b05725b35655f62ec22c46aa201da7586d064
acton <actondong@gmail.com>
2015-12-14 16:22:59 -0800

    .

---
+++ src/bigDataProcess/bigDataProcesser.java
@@ -0,0 +1,31 @@
+package bigDataProcess;
+
+public class bigDataProcesser {
+	
+	
+	
+	
+	public static boolean login(String name, String password){
+		
+		return false;
+	}
+	
+	public static boolean addUser(String name, String password){
+	
+		return false;
+	}
+	
+	public static void getRecommendation(String name){
+		
+		return;
+	}
+	
+	public static void rate(String name, String bookId){
+		
+	}
+	
+	
+	
+}
+
+
